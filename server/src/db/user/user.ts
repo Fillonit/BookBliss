5b4672cd1627bec7cdfcfe107b407c6df61c054e
Fillonit <fillonit.ibishi@versoaltima.com>
2024-03-12 13:20:59 +0100

    File structure update

---
+++ server/src/db/user/user.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+});
