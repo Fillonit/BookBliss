b5555b870fa7d3273b4f8afb95d43a3fc85be43a
Fillonit Ibishi <filloniti@gmail.com>
2024-03-17 22:01:03 +0100

    Add react-router-dom and create BookDetails and Home components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,32 @@
-// import "./App.css";
+// import React from "react";
+// import { Analytics } from "@vercel/analytics/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "@/pages/home";
+// import { useState } from "react";
+
 function App() {
+	// const [userId, setUserId] = useState(localStorage.getItem("userId"));
 	return (
-		<>
-			<h1 className="text-3xl font-bold underline text-red-300 bg-white">
-				BookBliss
-			</h1>
-		</>
+		<Router>
+			<div className="min-h-screen flex flex-col ">
+				{/* {!window.location.pathname.includes("/dashboard") && ( */}
+				{/* <Navbar /> */}
+				{/* )} */}
+				<Routes>
+					<Route path="/" element={<Home />} />
+					{/* <Route
+						path="/login"
+						element={<PublicRoute element={<LoginPage />} />}
+					/>
+					<Route
+						path="/dashboard/:table"
+						element={<PrivateRoute element={<Dashboard />} />}
+					/> */}
+				</Routes>
+				{/* {!window.location.pathname.includes("/dashboard") && <Footer />} */}
+			</div>
+			{/* <DarkThemeToggle className="fixed bottom-2 right-2 text-white dark:text-white bg-purple-400 hover:bg-purple-600 dark:hover:bg-purple-600 ring-0 focus:ring-0 dark:focus:ring-0 dark:ring-0" /> */}
+		</Router>
 	);
 }
 
---
+++ client/src/pages/home.tsx
@@ -0,0 +1,14 @@
+// import "./App.css";
+import BookCard from "@/components/Book/BookCard";
+function App() {
+	return (
+		<>
+			<h1 className="text-3xl font-bold underline text-red-300 bg-white">
+				BookBliss - HOME12313
+			</h1>
+			<BookCard />
+		</>
+	);
+}
+
+export default App;
