6aa6ba29215c9e9258b13d00356b95cf842e7247
Fillonit Ibishi <filloniti@gmail.com>
2024-03-18 19:45:03 +0100

    Add Contact model and UI components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,23 @@
 // import React from "react";
 // import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { DarkThemeToggle, Flowbite } from "flowbite-react";
 import Home from "@/pages/home";
+import Navbar from "@/components/Navbar";
 // import { useState } from "react";
 
 function App() {
 	// const [userId, setUserId] = useState(localStorage.getItem("userId"));
 	return (
 		<Router>
-			<div className="min-h-screen flex flex-col ">
-				{/* {!window.location.pathname.includes("/dashboard") && ( */}
-				{/* <Navbar /> */}
-				{/* )} */}
-				<Routes>
-					<Route path="/" element={<Home />} />
-					{/* <Route
+			<Flowbite>
+				<div className="min-h-screen flex flex-col ">
+					{/* {!window.location.pathname.includes("/dashboard") && ( */}
+					<Navbar />
+					{/* )} */}
+					<Routes>
+						<Route path="/" element={<Home />} />
+						{/* <Route
 						path="/login"
 						element={<PublicRoute element={<LoginPage />} />}
 					/>
@@ -22,10 +25,11 @@ function App() {
 						path="/dashboard/:table"
 						element={<PrivateRoute element={<Dashboard />} />}
 					/> */}
-				</Routes>
-				{/* {!window.location.pathname.includes("/dashboard") && <Footer />} */}
-			</div>
-			{/* <DarkThemeToggle className="fixed bottom-2 right-2 text-white dark:text-white bg-purple-400 hover:bg-purple-600 dark:hover:bg-purple-600 ring-0 focus:ring-0 dark:focus:ring-0 dark:ring-0" /> */}
+					</Routes>
+					{/* {!window.location.pathname.includes("/dashboard") && <Footer />} */}
+				</div>
+				<DarkThemeToggle className="fixed bottom-2 right-2 text-white dark:text-white bg-purple-400 hover:bg-purple-600 dark:hover:bg-purple-600 ring-0 focus:ring-0 dark:focus:ring-0 dark:ring-0" />
+			</Flowbite>
 		</Router>
 	);
 }
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -3,9 +3,9 @@ import BookCard from "@/components/Book/BookCard";
 function App() {
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline text-red-300 bg-white">
+			{/* <h1 className="text-3xl font-bold underline text-red-300 bg-white">
 				BookBliss - HOME12313
-			</h1>
+			</h1> */}
 			<BookCard />
 		</>
 	);
