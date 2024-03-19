02dd024cb98544ed15e465b009b9c08b83cf1bea
Fillonit <fillonit.ibishi@versoaltima.com>
2024-03-19 12:07:31 +0100

    Fixed React routing with public and private routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,21 +2,34 @@
 // import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { DarkThemeToggle, Flowbite } from "flowbite-react";
+
+// ? PAGES
 import Home from "@/pages/Homepage";
+import Login from "@/components/Auth/Login";
+import Register from "@/components/Auth/Register";
+import AboutUs from "@/components/About/about";
+// ? COMPONENTS
 import Navbar from "@/components/Layout/Navbar";
 import BookList from "@/components/Book/BookList";
-import AboutUs from "@/components/About/about"; 
-import Login from "./components/User/Login";
-// import { useState } from "react";
-
-// import "./App.css";
 import Footer from "@/components/Layout/Footer";
-import Register from "./components/User/Register";
-// import Hero from "@/components/Book/Hero";
+
+import { PublicRoute, PrivateRoute } from "@/util/routeHandler";
 function App() {
 	// const [userId, setUserId] = useState(localStorage.getItem("userId"));
 	return (
 		<Router>
+			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossorigin
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Vibes&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
 			<Flowbite>
 				<div className="min-h-screen flex flex-col ">
 					{/* {!window.location.pathname.includes("/dashboard") && ( */}
@@ -28,22 +41,24 @@ function App() {
 						<Route path="/about" element={<AboutUs />} />
 						<Route
 							path="/login"
-							element={<Login />}
+							element={<PublicRoute element={<Login />} />}
 						/>
 						<Route
 							path="/register"
-							element={<Register />}
-							/>
-						{/* <Route
-							path="/dashboard/:table"
-							element={<PrivateRoute element={<Dashboard />} />}
-						/>  */}
+							element={<PublicRoute element={<Register />} />}
+						/>
+						<Route
+							path="/admin"
+							element={<PrivateRoute element={<BookList />} />}
+						/>
 					</Routes>
-					{!window.location.pathname.includes("/dashboard") && !window.location.pathname.includes("/login") && !window.location.pathname.includes("/register") && (
-						<Footer />
-					)}
+					{!window.location.pathname.includes("/dashboard") &&
+						!window.location.pathname.includes("/login") &&
+						!window.location.pathname.includes("/register") && (
+							<Footer />
+						)}
 				</div>
-				<DarkThemeToggle className="fixed bottom-2 right-2 text-white dark:text-white bg-purple-400 hover:bg-purple-600 dark:hover:bg-purple-600 ring-0 focus:ring-0 dark:focus:ring-0 dark:ring-0" />
+				<DarkThemeToggle className="fixed bottom-2 right-2 text-white dark:text-white bg-amber-700 hover:bg-amber-900 dark:hover:bg-amber-600 ring-0 focus:ring-0 dark:focus:ring-0 dark:ring-0" />
 			</Flowbite>
 		</Router>
 	);
