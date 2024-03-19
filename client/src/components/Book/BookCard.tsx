55eea3e3f617430765d8fdbae7d90bcb6420e5b4
Fillonit <fillonit.ibishi@versoaltima.com>
2024-03-19 12:08:35 +0100

    Testing book cards and layouts, added custom fonts

--- client/src/components/Book/BookCard.tsx
+++ client/src/components/Book/BookCard.tsx
@@ -1,9 +1,60 @@
-import { Button } from "@/components/ui/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoodreads } from "@fortawesome/free-brands-svg-icons";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-export default function Home() {
+interface BookCardProps {
+	id: number;
+	title: string;
+	author: string;
+	description: string;
+	image: string;
+	rating: number;
+	popularity: number;
+	price: number;
+	genre: string;
+}
+
+export default function BookCard({ book }: BookCardProps) {
 	return (
-		<div>
-			<Button>Click me</Button>
+		<div
+			className="w-full max-w-sm border border-gray-200 rounded-lg shadow relative overflow-hidden min-h-[30vh] bg-center bg-cover"
+			style={{ backgroundImage: `url(${book.image})` }}
+		>
+			<div className="px-5 pb-5 bg-white bg-opacity-0 hover:bg-opacity-90 transition-all duration-500 absolute inset-0 flex flex-col justify-end space-y-4">
+				<a href="#">
+					<h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mt-8">
+						{book.title}
+					</h5>
+				</a>
+				<div className="flex items-center mt-2.5 mb-5">
+					<div className="flex items-center space-x-1 rtl:space-x-reverse">
+						<FontAwesomeIcon
+							icon={faStar}
+							className="text-yellow-300"
+						/>
+					</div>
+					<span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
+						{book.rating}
+					</span>
+				</div>
+				<div className="flex items-center justify-between mb-4">
+					<span className="text-3xl font-bold text-gray-900 dark:text-white">
+						${book.price}
+					</span>
+					<a
+						href="#"
+						className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					>
+						Add to cart
+					</a>
+				</div>
+				<a href={book.id} className="absolute bottom-4 right-4">
+					<FontAwesomeIcon
+						icon={faGoodreads}
+						className="text-gray-900 dark:text-white text-xl"
+					/>
+				</a>
+			</div>
 		</div>
 	);
 }
--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,16 +1,41 @@
 // import "./App.css";
-// import BookCard from "@/components/Book/BookCard";
+import BookCard from "@/components/Book/BookCard";
 import Hero from "@/components/Home/Hero";
+
+// import BookImage from "@/assets/book1.jpg";
+
+interface BookCardProps {
+	id: number;
+	title: string;
+	author: string;
+	description: string;
+	image: string;
+	rating: number;
+	popularity: number;
+	price: number;
+	genre: string;
+}
+
+const book: BookCardProps = {
+	id: 1,
+	title: `Book Title`,
+	author: "Author Name",
+	description:
+		"Description of the book goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod lorem non posuere commodo.",
+	image: `https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQPKlQbYym2c7Ja59ztwDCkV7wGBOeNi-71zGf-IK-4lWv205wB`,
+	rating: Math.floor(Math.random() * 5) + 1,
+	popularity: Math.floor(Math.random() * 1000),
+	price: Math.floor(Math.random() * 100) + 1,
+	genre: "Fantasy",
+};
 function App() {
 	return (
 		<>
-			{/* <h1 className="text-3xl font-bold underline text-red-300 bg-white">
-				BookBliss - HOME12313
-			</h1> */}
 			<Hero />
-			{/* <BookCard /> */}
+			{/* <div className="flex flex-col items-center justify-center py-2">
+				<BookCard book={book} />
+			</div> */}
 		</>
 	);
 }
-
 export default App;
