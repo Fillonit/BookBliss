024158db41e3938f4eba788c0f83c6acb9b8188b
Fillonit Ibishi <filloniti@gmail.com>
2024-03-22 23:26:12 +0100

    Refactor ratelimitMiddleware to accept maxRequests and windowSize parameters

--- server/src/middleware/ratelimit.ts
+++ server/src/middleware/ratelimit.ts
@@ -8,28 +8,33 @@ const redisConfig: RedisConfigNodejs = {
 };
 
 const redis = new Redis(redisConfig);
-const ratelimit = new Ratelimit({
-	redis,
-	limiter: Ratelimit.slidingWindow(10, "3s"),
-	analytics: false,
-});
-
-export const ratelimitMiddleware = async (
-	req: express.Request,
-	res: express.Response,
-	next: express.NextFunction
+export const ratelimitMiddleware = (
+	maxRequests: number,
+	windowSize: number
 ) => {
-	const ip = req.headers["x-forwarded-for"]?.toString() ?? "";
-	console.log(ip);
-	const { success, reset } = await ratelimit.limit(ip);
-	if (!success) {
-		const now = new Date().getTime();
-		const retryAfter = Math.floor((Number(reset) - now) / 1000);
-		return res.status(429).setHeader("Retry-After", retryAfter).json({
-			message: "Too many requests",
-			reset,
-		});
-	} else {
-		next();
-	}
+	const ratelimit = new Ratelimit({
+		redis,
+		limiter: Ratelimit.slidingWindow(maxRequests, `${windowSize}s`),
+		analytics: false,
+	});
+
+	return async (
+		req: express.Request,
+		res: express.Response,
+		next: express.NextFunction
+	) => {
+		const ip = req.headers["x-forwarded-for"]?.toString() ?? "";
+		console.log(ip);
+		const { success, reset } = await ratelimit.limit(ip);
+		if (!success) {
+			const now = new Date().getTime();
+			const retryAfter = Math.floor((Number(reset) - now) / 1000);
+			return res.status(429).setHeader("Retry-After", retryAfter).json({
+				message: "Too many requests",
+				reset,
+			});
+		} else {
+			next();
+		}
+	};
 };
--- server/src/router/authentication.ts
+++ server/src/router/authentication.ts
@@ -6,6 +6,6 @@ import { ratelimitMiddleware } from "../middleware/ratelimit";
 
 export default (router: express.Router) => {
 	router.post("/register", register);
-	router.post("/login", login);
-	router.delete("/logout/:id", logout);
+	router.post("/login", ratelimitMiddleware(3, 30), login);
+	router.delete("/logout/:id", ratelimitMiddleware(3, 30), logout);
 };
