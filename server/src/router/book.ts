959488ba18408207e939cad6a14234e1b85c082d
arlindz <arlindz123@gmail.com>
2024-03-23 01:19:40 +0100

    Fix user creation bug and add new book routes

--- server/src/router/book.ts
+++ server/src/router/book.ts
@@ -1,9 +1,13 @@
 import express from "express";
 
-import { getBooks } from "../controller/book/book";
+import { getBooks, getBook, updateBook, createBook } from "../controller/book/book";
+import upload from "@/util/fileSaver";
 
 import { ratelimitMiddleware } from "../middleware/ratelimit";
 
 export default (router: express.Router) => {
 	router.get("/books", getBooks);
+	router.get("/books/:id", getBook);
+	router.put("/books/:id", updateBook);
+	router.post("/books", upload.fields([{name: 'pdf', maxCount: 1}, {name: 'cover', maxCount: 1}]), createBook);
 };
