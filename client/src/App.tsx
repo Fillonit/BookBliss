076e8faedfcb67bc6ae0216b2f9325c70c745142
Fillonit Ibishi <filloniti@gmail.com>
2024-04-11 18:59:26 +0200

    Refactor UI components and add new features

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,68 +1,78 @@
 // import React from "react";
 // import { Analytics } from "@vercel/analytics/react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { DarkThemeToggle, Flowbite } from "flowbite-react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { DarkThemeToggle, Flowbite } from 'flowbite-react'
+import { ThemeProvider } from '@/components/theme-provider'
 
 // ? PAGES
-import Home from "@/pages/Homepage";
-import Login from "@/components/Auth/Login";
-import Register from "@/components/Auth/Register";
-import AboutUs from "@/components/About/about";
+import Home from '@/pages/Homepage'
+import Login from '@/components/Auth/Login'
+import Register from '@/components/Auth/Register'
+import AboutUs from '@/components/About/about'
+import Dashboard from '@/components/Admin/Dashboard'
 
 // ? COMPONENTS
-import Navbar from "@/components/Layout/Navbar";
-import BookList from "@/components/Book/BookList";
-import Footer from "@/components/Layout/Footer";
+import Navbar from '@/components/Layout/Navbar'
+import BookList from '@/components/Book/BookList'
+import Footer from '@/components/Layout/Footer'
 
-import { PublicRoute, PrivateRoute } from "@/util/routeHandler";
+import { PublicRoute, PrivateRoute } from '@/util/routeHandler'
 function App() {
-	// const [userId, setUserId] = useState(localStorage.getItem("userId"));
-	return (
-		<Router>
-			<head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Vibes&display=swap"
-					rel="stylesheet"
-				/>
-			</head>
-			<Flowbite>
-				<div className="min-h-screen flex flex-col ">
-					{/* {!window.location.pathname.includes("/dashboard") && ( */}
-					<Navbar />
-					{/* )} */}
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/books" element={<BookList />} />
-						<Route path="/about" element={<AboutUs />} />
-						<Route
-							path="/login"
-							element={<PublicRoute element={<Login />} />}
-						/>
-						<Route
-							path="/register"
-							element={<PublicRoute element={<Register />} />}
-						/>
-						<Route
-							path="/admin"
-							element={<PrivateRoute element={<BookList />} />}
-						/>
-					</Routes>
-					{!window.location.pathname.includes("/dashboard") &&
-						!window.location.pathname.includes("/login") &&
-						!window.location.pathname.includes("/register") && (
-							<Footer />
-						)}
-				</div>
-				<DarkThemeToggle className="fixed bottom-2 right-2 text-white dark:text-white bg-amber-700 hover:bg-amber-900 dark:hover:bg-amber-600 ring-0 focus:ring-0 dark:focus:ring-0 dark:ring-0" />
-			</Flowbite>
-		</Router>
-	);
+    // const [userId, setUserId] = useState(localStorage.getItem("userId"));
+    return (
+        <Router>
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Vibes&display=swap"
+                    rel="stylesheet"
+                />
+            </head>
+            <Flowbite>
+                <ThemeProvider
+                    defaultTheme="dark"
+                    storageKey="flowbite-theme-mode"
+                >
+                    <div className="min-h-screen flex flex-col ">
+                        {/* {!window.location.pathname.includes("/dashboard") && ( */}
+                        <Navbar />
+                        {/* )} */}
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/books" element={<BookList />} />
+                            <Route path="/about" element={<AboutUs />} />
+                            <Route
+                                path="/login"
+                                element={<PublicRoute element={<Login />} />}
+                            />
+                            <Route
+                                path="/register"
+                                element={<PublicRoute element={<Register />} />}
+                            />
+                            <Route
+                                path="/admin"
+                                element={
+                                    <PrivateRoute element={<BookList />} />
+                                }
+                            />
+                            <Route path="/dashboard" element={<Dashboard />} />
+                            <Route
+                                path="/dashboard/:table"
+                                element={<Dashboard />}
+                            />
+                        </Routes>
+                        {!window.location.pathname.includes('/dashboard') &&
+                            !window.location.pathname.includes('/login') &&
+                            !window.location.pathname.includes('/register') && (
+                                <Footer />
+                            )}
+                    </div>
+                    <DarkThemeToggle className="fixed bottom-2 right-2 text-white dark:text-white bg-amber-700 hover:bg-amber-900 dark:hover:bg-amber-600 ring-0 focus:ring-0 dark:focus:ring-0 dark:ring-0" />
+                </ThemeProvider>
+            </Flowbite>
+        </Router>
+    )
 }
 
-export default App;
+export default App
