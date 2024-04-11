0b10a06a07251081e193e92a3726bd055e4fdea7
Fillonit Ibishi <filloniti@gmail.com>
2024-04-11 19:04:51 +0200

    Refactor BookList component and update dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,8 +10,8 @@ import morgan from "morgan";
 import session from "express-session";
 
 /* ! Google OAuth packages ! */
-// import passport from "passport";
-// import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 
 dotenv.config();
 const { PORT, MONGO_URL, NODE_ENV } = process.env;
@@ -24,114 +24,114 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(compression());
-// app.use(
-// 	session({
-// 		secret: process.env.SESSION_SECRET!,
-// 		resave: false,
-// 		saveUninitialized: true,
-// 		cookie: { secure: "auto" },
-// 	})
-// );
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET!,
+		resave: false,
+		saveUninitialized: true,
+		cookie: { secure: "auto" },
+	})
+);
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 import { authentication, random } from "./util";
 
-// passport.use(
-// 	new GoogleStrategy(
-// 		{
-// 			clientID: process.env.GOOGLE_OAUTH_CLIENT_ID!,
-// 			clientSecret: process.env.GOOGLE_OAUTH_SECRET!,
-// 			callbackURL: "/auth/google/callback",
-// 		},
-// 		async (accessToken, refreshToken, profile, done) => {
-// 			let sessionToken = await authentication(accessToken, profile.id);
-// 			const existingUser = await prisma.user.findFirst({
-// 				where: {
-// 					OR: [
-// 						{ email: profile.emails[0].value },
-// 						{ googleId: profile.id },
-// 						{ sessionToken: accessToken },
-// 					],
-// 				},
-// 			});
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_OAUTH_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_OAUTH_SECRET!,
+			callbackURL: "/auth/google/callback",
+		},
+		async (accessToken, refreshToken, profile, done) => {
+			let sessionToken = await authentication(accessToken, profile.id);
+			const existingUser = await prisma.user.findFirst({
+				where: {
+					OR: [
+						{ email: profile.emails[0].value },
+						{ googleId: profile.id },
+						{ sessionToken: accessToken },
+					],
+				},
+			});
 
-// 			if (!existingUser) {
-// 				try {
-// 					await prisma.user.create({
-// 						data: {
-// 							email: profile.emails[0].value,
-// 							name: profile.displayName,
-// 							role: "user",
-// 							avatar: profile.photos[0].value,
-// 							password: profile.id,
-// 							googleId: profile.id,
-// 							salt: random(),
-// 							sessionToken: accessToken,
-// 						},
-// 					});
-// 				} catch (error) {
-// 					console.error(error);
-// 					return done(error, null);
-// 				}
-// 			}
+			if (!existingUser) {
+				try {
+					await prisma.user.create({
+						data: {
+							email: profile.emails[0].value,
+							name: profile.displayName,
+							role: "user",
+							avatar: profile.photos[0].value,
+							password: profile.id,
+							googleId: profile.id,
+							salt: random(),
+							sessionToken: accessToken,
+						},
+					});
+				} catch (error) {
+					console.error(error);
+					return done(error, null);
+				}
+			}
 
-// 			return done(null, profile);
-// 		}
-// 	)
-// );
+			return done(null, profile);
+		}
+	)
+);
 
-// passport.serializeUser((user, done) => {
-// 	done(null, user);
-// });
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
 
-// passport.deserializeUser((user, done) => {
-// 	done(null, user);
-// });
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
 
-// app.get(
-// 	"/auth/google",
-// 	passport.authenticate("google", { scope: ["profile", "email"] })
-// );
+app.get(
+	"/auth/google",
+	passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
 import { prisma } from "./db/client";
 
-// interface GoogleUser {
-// 	id: string;
-// 	displayName: string;
-// 	emails: { value: string; verified: boolean }[];
-// 	photos: { value: string }[];
-// }
+interface GoogleUser {
+	id: string;
+	displayName: string;
+	emails: { value: string; verified: boolean }[];
+	photos: { value: string }[];
+}
 
-// app.get(
-// 	"/auth/google/callback",
-// 	passport.authenticate("google", {
-// 		failureRedirect: "http://localhost:5173/login",
-// 	}),
-// 	async (req: express.Request, res: express.Response) => {
-// 		try {
-// 			const googleUser = req.user as GoogleUser;
-// 			// const user = await prisma.user.create({
-// 			// 	data: {
-// 			// 		email: googleUser.emails[0].value,
-// 			// 		name: googleUser.displayName,
-// 			// 		role: "user",
-// 			// 		avatar: googleUser.photos[0].value,
-// 			// 		password: googleUser.id,
-// 			// 	},
-// 			// });
-// 			res.redirect(`http://localhost:5173/login?id=${googleUser.id}`);
-// 		} catch (error) {
-// 			console.error(error);
-// 			if (error.includes("prisma.user.create()")) {
-// 				res.status(500).json({ error: "Failed to create user" });
-// 			} else {
-// 				res.status(500).json({ error: "Failed to authenticate user" });
-// 			}
-// 		}
-// 	}
-// );
+app.get(
+	"/auth/google/callback",
+	passport.authenticate("google", {
+		failureRedirect: "http://localhost:5173/login",
+	}),
+	async (req: express.Request, res: express.Response) => {
+		try {
+			const googleUser = req.user as GoogleUser;
+			// const user = await prisma.user.create({
+			// 	data: {
+			// 		email: googleUser.emails[0].value,
+			// 		name: googleUser.displayName,
+			// 		role: "user",
+			// 		avatar: googleUser.photos[0].value,
+			// 		password: googleUser.id,
+			// 	},
+			// });
+			res.redirect(`http://localhost:5173/login?id=${googleUser.id}`);
+		} catch (error) {
+			console.error(error);
+			if (error.includes("prisma.user.create()")) {
+				res.status(500).json({ error: "Failed to create user" });
+			} else {
+				res.status(500).json({ error: "Failed to authenticate user" });
+			}
+		}
+	}
+);
 
 app.use(
 	cors({
