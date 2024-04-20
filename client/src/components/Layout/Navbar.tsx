6537980b967147ff26c1413d63837b28dd6108d7
Fillonit Ibishi <filloniti@gmail.com>
2024-04-20 23:32:31 +0200

    Update Navbar menu item label from "Getting started" to "Explore"

--- client/src/components/Layout/Navbar.tsx
+++ client/src/components/Layout/Navbar.tsx
@@ -169,14 +169,14 @@ function NavbarComponent() {
                         <NavigationMenuList>
                             <NavigationMenuItem>
                                 <NavigationMenuTrigger>
-                                    Getting started
+                                    Explore
                                 </NavigationMenuTrigger>
                                 <NavigationMenuContent>
                                     <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                                         <li className="row-span-3">
                                             <NavigationMenuLink asChild>
                                                 <a
-                                                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b   from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                                                     href="/"
                                                 >
                                                     <Icons.logo className="h-6 w-6 text-amber-500" />
