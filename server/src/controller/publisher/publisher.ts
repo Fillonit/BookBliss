67f3e4eef1163511d005f2b7621c72d7bbeaa9ed
Fillonit Ibishi <filloniti@gmail.com>
2024-05-26 18:54:27 +0200

    Add CRUD operations for publishers in the server controller

--- server/src/controller/publisher/publisher.ts
+++ server/src/controller/publisher/publisher.ts
@@ -6,5 +6,76 @@ export const getPublishers = async (
 	res: express.Response
 ) => {
 	const publishers = await prisma.publisher.findMany();
-	res.status(200).json({message: "Successfully fetched data", data: publishers});
+	res.status(200).json({
+		message: "Successfully fetched data",
+		data: publishers,
+	});
+};
+
+export const getPublisherById = async (
+	req: express.Request,
+	res: express.Response
+) => {
+	const { id } = req.params;
+	const publisher = await prisma.publisher.findUnique({
+		where: {
+			id: parseInt(id),
+		},
+	});
+	res.status(200).json({
+		message: "Successfully fetched data",
+		data: publisher,
+	});
+};
+
+export const createPublisher = async (
+	req: express.Request,
+	res: express.Response
+) => {
+	const { name } = req.body;
+	const publisher = await prisma.publisher.create({
+		data: {
+			name,
+		},
+	});
+	res.status(200).json({
+		message: "Successfully created publisher",
+		data: publisher,
+	});
+};
+
+export const updatePublisher = async (
+	req: express.Request,
+	res: express.Response
+) => {
+	const { id } = req.params;
+	const { name } = req.body;
+	const publisher = await prisma.publisher.update({
+		where: {
+			id: parseInt(id),
+		},
+		data: {
+			name,
+		},
+	});
+	res.status(200).json({
+		message: "Successfully updated publisher",
+		data: publisher,
+	});
+};
+
+export const deletePublisher = async (
+	req: express.Request,
+	res: express.Response
+) => {
+	const { id } = req.params;
+	const publisher = await prisma.publisher.delete({
+		where: {
+			id: parseInt(id),
+		},
+	});
+	res.status(200).json({
+		message: "Successfully deleted publisher",
+		data: publisher,
+	});
 };
